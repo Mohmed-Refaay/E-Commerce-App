cf5ad41c56832990ec8341d3a157860a15a06355
unknown <mohamedamrrefaay@gmail.com>
2020-10-03 04:21:28 +0200

    handling 404

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 // Routes
-const adminRoutes = require("./routes/admin")
-const shopRoutes = require("./routes/shop")
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
 
 const app = express();
 
@@ -11,5 +11,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(adminRoutes);
 app.use(shopRoutes);
+
+app.use((req, res) => {
+  res.status(404).send("<h1>Page not found</h1>");
+});
 
 app.listen(8000);
